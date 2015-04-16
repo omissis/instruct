f7fa95dd2f1d5125e017c409e85a151e00e04162
omissis <claudi0.beatric3@gmail.com>
2015-04-17 00:55:07 +0200

    Added more testcase for Rule class.

--- tests/RuleTest.php
+++ tests/RuleTest.php
@@ -14,11 +14,8 @@ class RuleTest extends TestCase
         $this->conditions = $this->prophesize('FOD\Instruct\Condition\ConditionInterface');
         $this->actions = $this->prophesize('FOD\Instruct\Action\ActionInterface');
         $this->contextBuilder = $this->prophesize('FOD\Instruct\Context\ContextBuilderInterface');
-    }
 
-    public function testInstatiation()
-    {
-        new Rule(
+        $this->rule = new Rule(
             $this->events->reveal(),
             $this->conditions->reveal(),
             $this->actions->reveal(),
@@ -34,19 +31,42 @@ class RuleTest extends TestCase
         $this->contextBuilder->addEvent($event->reveal())->willReturn($this->contextBuilder->reveal())->shouldBeCalledTimes(1);
         $this->contextBuilder->getContext()->willReturn($context->reveal())->shouldBeCalledTimes(1);
 
-        $rule = new Rule(
-            $this->events->reveal(),
-            $this->conditions->reveal(),
-            $this->actions->reveal(),
-            $this->contextBuilder->reveal()
-        );
-
         $this->events->contain($event->reveal())->willReturn(true)->shouldBeCalledTimes(1);
 
         $this->conditions->verify($context->reveal())->willReturn(true)->shouldBeCalledTimes(1);
 
         $this->actions->execute($context->reveal())->willReturn(null)->shouldBeCalledTimes(1);
 
-        $rule->apply($event->reveal());
+        $this->rule->apply($event->reveal());
     }
+
+    public function testDoesntApplyOnUnregisteredEvent()
+    {
+        $event = $this->prophesize('FOD\Instruct\Event\EventInterface');
+
+        $this->events->contain($event->reveal())->willReturn(false)->shouldBeCalledTimes(1);
+
+        $this->conditions->verify()->shouldBeCalledTimes(0);
+
+        $this->actions->execute()->shouldBeCalledTimes(0);
+
+        $this->rule->apply($event->reveal());
+    }
+
+    public function testDoesntApplyOnUnmatchedConditions()
+        {
+            $event = $this->prophesize('FOD\Instruct\Event\EventInterface');
+            $context = $this->prophesize('FOD\Instruct\Context\ContextInterface');
+
+            $this->contextBuilder->addEvent($event->reveal())->willReturn($this->contextBuilder->reveal())->shouldBeCalledTimes(1);
+            $this->contextBuilder->getContext()->willReturn($context->reveal())->shouldBeCalledTimes(1);
+
+            $this->events->contain($event->reveal())->willReturn(true)->shouldBeCalledTimes(1);
+
+            $this->conditions->verify($context->reveal())->willReturn(false)->shouldBeCalledTimes(1);
+
+            $this->actions->execute()->shouldBeCalledTimes(0);
+
+            $this->rule->apply($event->reveal());
+        }
 }
